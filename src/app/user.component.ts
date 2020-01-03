81c06ca88168d6377dc1c6dd619bd47ec4640cfa
Mike Ajala <agmt5989@gmail.com>
2020-01-03 01:30:35 +0100

    thought something broke

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-user',
-    templateUrl: `<h1>Well wait<h1>
+    template: `<h1>Well wait<h1>
     <p>What kind of user begins his life with a component?</p>`
 })
 export class UserComponent {}
