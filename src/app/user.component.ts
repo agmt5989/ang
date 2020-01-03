a6e41263e937811e9d7fac6fae02d8d964b0ca9c
Mike Ajala <agmt5989@gmail.com>
2020-01-03 01:41:31 +0100

    my v-model

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class UserComponent {
     name = 'Michael'
 
-    onUserInput(event) {
-        this.name = event.target.value;
+    onUserInput(e) {
+        this.name = e.target.value;
     }
 }
