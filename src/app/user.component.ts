bd3637b14d86ca76da8f38f69ca0d8189ae7245d
Mike Ajala <agmt5989@gmail.com>
2020-01-03 01:49:02 +0100

    faster approach, thanks @formsModule

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -3,14 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-user',
     template: `
-    <input type="text" (input)="onUserInput($event)" [value]=name> <!--value={{name}}--> 
+    <input type="text" [(ngModel)]="name">
     <h1>Well, wait up {{name}}<h1>
     <p>What kind of user begins his life with a component?</p>`
 })
 export class UserComponent {
     name = 'Michael'
-
-    onUserInput(e) {
-        this.name = e.target.value;
-    }
 }
