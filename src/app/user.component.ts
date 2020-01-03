214a868d17dacfe271f73cc3f56eabe74e8c5f71
Mike Ajala <agmt5989@gmail.com>
2020-01-03 21:56:01 +0100

    awesome thing with props

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-user',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
     <p>What kind of user begins his life with a component?</p>`
 })
 export class UserComponent {
-    name = 'Michael'
+    @Input() name = 'Michael';
 }
