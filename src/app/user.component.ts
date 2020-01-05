284336e7d71372536ce565124a93ee37e8926a3d
Mike Ajala <agmt5989@gmail.com>
2020-01-05 22:11:58 +0100

    bootstrap not working imo

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -7,14 +7,24 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     <!-- <input type="text" [(ngModel)]="name"> -->
     <h1>Well, wait up {{name}}<h1>
     <p>What kind of user begins his life with a component?</p>
+    <p>
+      {{number}}
+    <button (click)="increaseNumber()" class="btn btn-primary btn-large"></button>
+    </p>
     <app-user-detail></app-user-detail>
     `
 })
 export class UserComponent {
+    number = 0;
+
     @Input() name = 'Michael';
     @Output() nameChanged = new EventEmitter<string>();
 
     onUserInput(e) {
       this.name = e.target.value;
+    }
+
+    increaseNumber() {
+      this.number++;
     }
 }
